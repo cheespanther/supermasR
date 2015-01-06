9fd4e5062e17a697cbd81ca4f88aa5805f2dc2a3
Iskar Waluyo <iskarwaluyo@gmail.com>
2015-01-05 19:22:33 -0600

    Update mensualcumulativo.R

--- mensualcumulativo.R
+++ mensualcumulativo.R
@@ -32,15 +32,18 @@ library("lubridate")
     c4 <- as.matrix(subset (soloventas, soloventas > 100 & soloventas <= 200))
     c5 <- as.matrix(subset (soloventas, soloventas > 200 & soloventas <= 400))
     c6 <- as.matrix(subset (soloventas, soloventas > 400))
+    
     cuentas1 <- nrow(c1)
     cuentas2 <- nrow(c2)
     cuentas3 <- nrow(c3)
     cuentas4 <- nrow(c4)
     cuentas5 <- nrow(c5)
     cuentas6 <- nrow(c6)
+    
     Frecuencia <- c(cuentas1, cuentas2, cuentas3, cuentas4, cuentas5, cuentas6)
     Clases <- c("Menor a $20.00", "$21.00 a $50.00", "$51.00 a $100.00",
                   "$101.00 a $200.00", "$201.00 a $400.00", "Mayor a $400.00")
+                  
     tabla <- data.frame(Clases, Frecuencia)
     tablabonita <- format (tabla, width = 20, justify = "right")
 
